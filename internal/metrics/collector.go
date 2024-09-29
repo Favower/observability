aa9b73894365b20b251f66682d3934ecbb8fbca4
Favower <Amir.umakhanov@yandex.ru>
2024-09-30 01:23:01 +0300

    -

--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -1,13 +1,13 @@
 package metrics
 
 import (
-	"fmt"
-	"net/http"
-	"time"
-	"runtime"
-	"math/rand"
-	"encoding/json"
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"time"
 )
 
 // MetricSender - интерфейс для отправки метрик.
@@ -20,60 +20,67 @@ type HTTPMetricSender struct {
 	ServerAddress string
 }
 
+// Metrics - структура для метрик.
 type Metrics struct {
 	ID    string   `json:"id"`              // имя метрики
 	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
 	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
 	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
- }
+}
 
+// NewHTTPMetricSender - конструктор для HTTPMetricSender.
 func NewHTTPMetricSender(serverAddress string) *HTTPMetricSender {
 	return &HTTPMetricSender{ServerAddress: serverAddress}
 }
 
+// SendMetric - метод для отправки метрик в формате JSON через HTTP.
 func (s *HTTPMetricSender) SendMetric(metricType, metricName string, value float64) error {
-    url := fmt.Sprintf("http://%s/value/", s.ServerAddress)
-    reqBody := Metrics{ID: metricName, MType: metricType}
-    if metricType == "gauge" {
-        reqBody.Value = &value
-    } else if metricType == "counter" {
-        reqBody.Delta = new(int64)
-        *reqBody.Delta = int64(value)
-    }
+	url := fmt.Sprintf("http://%s/value/", s.ServerAddress)
+	reqBody := Metrics{ID: metricName, MType: metricType}
 
-    reqBodyJSON, err := json.Marshal(reqBody)
-    if err != nil {
-        return fmt.Errorf("ошибка сериализации метрики: %v", err)
-    }
+	// Обработка значений в зависимости от типа метрики.
+	if metricType == "gauge" {
+		reqBody.Value = &value
+	} else if metricType == "counter" {
+		reqBody.Delta = new(int64)
+		*reqBody.Delta = int64(value)
+	}
 
-    req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(reqBodyJSON))
-    if err != nil {
-        return fmt.Errorf("ошибка создания запроса: %v", err)
-    }
+	reqBodyJSON, err := json.Marshal(reqBody)
+	if err != nil {
+		return fmt.Errorf("ошибка сериализации метрики: %v", err)
+	}
 
-    req.Header.Set("Content-Type", "application/json")
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        return fmt.Errorf("ошибка отправки метрики: %v", err)
-    }
-    defer resp.Body.Close()
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(reqBodyJSON))
+	if err != nil {
+		return fmt.Errorf("ошибка создания запроса: %v", err)
+	}
 
-    if resp.StatusCode != http.StatusOK {
-        return fmt.Errorf("сервер вернул некорректный статус: %v", resp.Status)
-    }
-    return nil
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("ошибка отправки метрики: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("сервер вернул некорректный статус: %v", resp.Status)
+	}
+	return nil
 }
 
-
+// Collector - структура для сбора метрик.
 type Collector struct {
 	pollCount int64
 }
 
+// NewCollector - конструктор для Collector.
 func NewCollector() *Collector {
 	return &Collector{}
 }
 
+// CollectAndSendMetrics - метод для сбора метрик и их отправки через указанный MetricSender.
 func (c *Collector) CollectAndSendMetrics(sender MetricSender, pollInterval, reportInterval time.Duration) {
 	ticker := time.NewTicker(pollInterval)
 	defer ticker.Stop()
@@ -98,7 +105,7 @@ func (c *Collector) CollectAndSendMetrics(sender MetricSender, pollInterval, rep
 	}
 }
 
-
+// collectMetrics - метод для сбора метрик о состоянии системы.
 func (c *Collector) collectMetrics() map[string]float64 {
 	var memStats runtime.MemStats
 	runtime.ReadMemStats(&memStats)
@@ -106,33 +113,33 @@ func (c *Collector) collectMetrics() map[string]float64 {
 	c.pollCount++
 
 	return map[string]float64{
-		"Alloc":        float64(memStats.Alloc),
-		"BuckHashSys":	float64(memStats.BuckHashSys),
-		"Frees":        float64(memStats.Frees),
+		"Alloc":         float64(memStats.Alloc),
+		"BuckHashSys":   float64(memStats.BuckHashSys),
+		"Frees":         float64(memStats.Frees),
 		"GCCPUFraction": memStats.GCCPUFraction,
-		"GCSys":        float64(memStats.GCSys),
-		"HeapAlloc":    float64(memStats.HeapAlloc),
-		"HeapIdle":     float64(memStats.HeapIdle),
-		"HeapInuse":    float64(memStats.HeapInuse),
-		"HeapObjects":  float64(memStats.HeapObjects),
-		"HeapReleased": float64(memStats.HeapReleased),
-		"HeapSys":      float64(memStats.HeapSys),
-		"LastGC":       float64(memStats.LastGC),
-		"Lookups":      float64(memStats.Lookups),
-		"MCacheInuse":  float64(memStats.MCacheInuse),
-		"MCacheSys":    float64(memStats.MCacheSys),
-		"MSpanInuse":   float64(memStats.MSpanInuse),
-		"MSpanSys":     float64(memStats.MSpanSys),
-		"NextGC":       float64(memStats.NextGC),
-		"NumForcedGC":  float64(memStats.NumForcedGC),
-		"NumGC":        float64(memStats.NumGC),
-		"OtherSys":     float64(memStats.OtherSys),
-		"PauseTotalNs": float64(memStats.PauseTotalNs),		
-		"StackInuse":   float64(memStats.StackInuse),
-		"StackSys":     float64(memStats.StackSys),
-		"Sys":          float64(memStats.Sys),
-		"TotalAlloc":   float64(memStats.TotalAlloc),
-		"PollCount":    float64(c.pollCount),
-		"RandomValue":	rand.Float64(),
+		"GCSys":         float64(memStats.GCSys),
+		"HeapAlloc":     float64(memStats.HeapAlloc),
+		"HeapIdle":      float64(memStats.HeapIdle),
+		"HeapInuse":     float64(memStats.HeapInuse),
+		"HeapObjects":   float64(memStats.HeapObjects),
+		"HeapReleased":  float64(memStats.HeapReleased),
+		"HeapSys":       float64(memStats.HeapSys),
+		"LastGC":        float64(memStats.LastGC),
+		"Lookups":       float64(memStats.Lookups),
+		"MCacheInuse":   float64(memStats.MCacheInuse),
+		"MCacheSys":     float64(memStats.MCacheSys),
+		"MSpanInuse":    float64(memStats.MSpanInuse),
+		"MSpanSys":      float64(memStats.MSpanSys),
+		"NextGC":        float64(memStats.NextGC),
+		"NumForcedGC":   float64(memStats.NumForcedGC),
+		"NumGC":         float64(memStats.NumGC),
+		"OtherSys":      float64(memStats.OtherSys),
+		"PauseTotalNs":  float64(memStats.PauseTotalNs),
+		"StackInuse":    float64(memStats.StackInuse),
+		"StackSys":      float64(memStats.StackSys),
+		"Sys":           float64(memStats.Sys),
+		"TotalAlloc":    float64(memStats.TotalAlloc),
+		"PollCount":     float64(c.pollCount),
+		"RandomValue":   rand.Float64(),
 	}
 }
